ff87d5dfc233b27f79a09b72ce672dd302c9574b
Beynar <arnaud@derbey.dev>
2025-03-27 19:08:13 +0100

    update package.json for publication on npl

---
+++ packages/server/tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts", "src/cloudflare/index.ts"],
+  splitting: true,
+  skipNodeModulesBundle: true,
+  dts: true,
+  bundle: true,
+  minifyIdentifiers: true,
+  minifySyntax: true,
+  minifyWhitespace: true,
+  platform: "node",
+  external: ["cloudflare:workers"],
+  keepNames: true,
+  minify: true,
+  sourcemap: true,
+  format: ["cjs", "esm"],
+  treeshake: true,
+  clean: true
+});
